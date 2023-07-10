3da76b4299eeb60e8cde66fe964b0bf405ac40bc
DuyNM0601 <manhduy6102@gmail.com>
2023-07-10 14:00:40 +0700

    unfix_payment

---
+++ src/main/java/com/example/miniProject/model/Payment.java
@@ -0,0 +1,46 @@
+package com.example.miniProject.model;
+
+
+
+import java.io.Serializable;
+
+
+public class Payment implements Serializable {
+    private String status;
+    private String message;
+    private String URL;
+
+    public Payment(){
+
+    }
+
+    public Payment(String status, String message, String URL) {
+        this.status = status;
+        this.message = message;
+        this.URL = URL;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+}
